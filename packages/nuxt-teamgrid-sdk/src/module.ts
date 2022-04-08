1df9a00c43629d6d0088a656e6a30fdf4536d8ec
Kubilay Melnikov <melnikov@medienagenten.de>
2022-04-08 22:18:53 +0200

    fix: wrong paths

--- packages/nuxt-teamgrid-sdk/src/module.ts
+++ packages/nuxt-teamgrid-sdk/src/module.ts
@@ -3,15 +3,15 @@ import { join, resolve } from "path";
 
 const teamGridSDKModule: Module = function () {
   this.addPlugin({
-    src: resolve(__dirname, "src/plugins/context.js"),
+    src: resolve(__dirname, "plugins/context.js"),
     fileName: join("teamgrid-shared", "plugins.context.js"),
   });
 
   this.extendBuild((config) => {
-    config.resolve.alias["~teamgrid-shared"] = resolve(__dirname, "src/");
+    config.resolve.alias["~teamgrid-shared"] = resolve(__dirname, "./");
   });
 };
 
 export default teamGridSDKModule;
 
-export const meta = require("./package.json");
+export const meta = require("./../package.json");
