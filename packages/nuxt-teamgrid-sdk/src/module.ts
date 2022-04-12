35c8e2e5fc187bf47de633900b8d442ba9f35ca2
Kubilay Melnikov <melnikov@medienagenten.de>
2022-04-12 09:19:43 +0200

    fix: try change module type

--- packages/nuxt-teamgrid-sdk/src/module.ts
+++ packages/nuxt-teamgrid-sdk/src/module.ts
@@ -13,19 +13,4 @@ const module: Module = function () {
 
 export default module
 
-export * from './module'
-export * from './plugins/plugin'
-export * from './api/teamGridSDK'
-export * from './api/types/Contact'
-export * from './api/types/List'
-export * from './api/types/Project'
-export * from './api/types/Service'
-export * from './api/types/Tag'
-export * from './api/types/Task'
-export * from './api/types/Team'
-export * from './api/types/TeamGridResponse'
-export * from './api/types/Time'
-export * from './api/types/User'
-export * from './api/types/Webhook'
-
 export const meta = require('./../package.json')
