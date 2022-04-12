ee512d842b45656872917c63dc4cedc8823467ae
Kubilay Melnikov <melnikov@medienagenten.de>
2022-04-12 09:40:47 +0200

    fix: try type defintion export

--- packages/nuxt-teamgrid-sdk/src/module.ts
+++ packages/nuxt-teamgrid-sdk/src/module.ts
@@ -15,6 +15,29 @@ export default module
 
 export const meta = require('./../package.json')
 
+import { TeamGridSDK } from '@nuxt-teamgrid-sdk/api/teamGridSDK'
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $teamGridSDK: TeamGridSDK
+  }
+}
+
+declare module '@nuxt/types' {
+  interface NuxtAppOptions {
+    $teamGridSDK: TeamGridSDK
+  }
+  interface Context {
+    $teamGridSDK: TeamGridSDK
+  }
+}
+
+declare module 'vuex/types/index' {
+  interface Store<S> {
+    $teamGridSDK: TeamGridSDK
+  }
+}
+
 export * from './module'
 export * from './plugins/plugin'
 export * from './api/teamGridSDK'
--- packages/nuxt-teamgrid-sdk/src/plugins/plugin.ts
+++ packages/nuxt-teamgrid-sdk/src/plugins/plugin.ts
@@ -1,27 +1,6 @@
 import { Plugin } from '@nuxt/types'
 import { TeamGridSDK } from '@nuxt-teamgrid-sdk/api/teamGridSDK'
 
-declare module 'vue/types/vue' {
-  interface Vue {
-    $teamGridSDK: TeamGridSDK
-  }
-}
-
-declare module '@nuxt/types' {
-  interface NuxtAppOptions {
-    $teamGridSDK: TeamGridSDK
-  }
-  interface Context {
-    $teamGridSDK: TeamGridSDK
-  }
-}
-
-declare module 'vuex/types/index' {
-  interface Store<S> {
-    $teamGridSDK: TeamGridSDK
-  }
-}
-
 const plugin: Plugin = (_, inject) => {
   const teamGridSDK = new TeamGridSDK()
   inject('teamGridSDK', teamGridSDK)
